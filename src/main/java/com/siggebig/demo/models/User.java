c6a4dd703a50cf78811aecabf78d075fd33eb752
SiggeAlfredsson <saorust@hotmail.com>
2024-02-16 17:48:43 +0100

    user model

---
+++ src/main/java/com/siggebig/demo/models/User.java
@@ -0,0 +1,37 @@
+package com.siggebig.demo.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany
+    private Set<User> followers = new HashSet<>();
+
+    @ManyToMany
+    private Set<User> following = new HashSet<>();
+
+
+//    soon to impl
+//    @OneToMany(mappedBy = "user")
+//    @JsonIgnoreProperties("user")
+//    private List<Project> projects = new ArrayList<>();
+
+
+
+}
