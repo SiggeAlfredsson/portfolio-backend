5a35db38adcd18810eb3c0517b7a28472392c59e
SiggeAlfredsson <saorust@hotmail.com>
2024-03-10 20:19:48 +0100

    test

--- UserAuth/src/main/java/org/siggebig/UserAuthApplication.java
+++ UserAuth/src/main/java/org/siggebig/UserAuthApplication.java
@@ -2,7 +2,7 @@ package org.siggebig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//test
 @SpringBootApplication
 public class UserAuthApplication {
     public static void main(String[] args) {
