adf82630994a3b6ef0880237e12fadca5a7a4376
SiggeAlfredsson <saorust@hotmail.com>
2024-04-17 23:23:35 +0200

    bugfix and commandlinerunner

--- UserAuth/src/main/java/org/siggebig/UserAuthApplication.java
+++ UserAuth/src/main/java/org/siggebig/UserAuthApplication.java
@@ -1,11 +1,37 @@
 package org.siggebig;
 
+import org.siggebig.models.User;
+import org.siggebig.repositorys.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//test
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
 @SpringBootApplication
 public class UserAuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserAuthApplication.class, args);
     }
+
+    @Bean
+    public CommandLineRunner run(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        return args -> {
+
+            // if db is empty, creates a user that always exists
+            if (!userRepository.existsById(1L)) {
+                User user = new User();
+                user.setId(1L);
+                user.setEmail("admin@domain.com");
+                user.setPassword("password");
+                String encodedPasswordUser = passwordEncoder.encode(user.getPassword());
+                user.setPassword(encodedPasswordUser);
+                userRepository.save(user);
+            }
+
+            System.out.println("-------initalized database-------");
+        };
+    }
 }
+
--- UserAuth/src/main/java/org/siggebig/models/LoginDto.java
+++ UserAuth/src/main/java/org/siggebig/models/LoginDto.java
@@ -1,11 +1,17 @@
 package org.siggebig.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class LoginDto {
     private String email;
     private String password;
+
+
 }
