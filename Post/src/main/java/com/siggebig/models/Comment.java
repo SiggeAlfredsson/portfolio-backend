99de3f05ac0113fe1d2dc19730823048fd5285af
SiggeAlfredsson <saorust@hotmail.com>
2024-04-23 22:47:18 +0200

    cmt upd

--- Post/src/main/java/com/siggebig/models/Comment.java
+++ Post/src/main/java/com/siggebig/models/Comment.java
@@ -2,12 +2,16 @@ package com.siggebig.models;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String text;
+
+    private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
